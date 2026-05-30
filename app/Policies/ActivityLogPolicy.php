43001b0feeff074add6efa354b85f277bdd0b672
damian <rezalkaryanto@gmail.com>
2026-05-30 20:26:13 +0700

    feat: shield policy & permissions for activity log

---
+++ app/Policies/ActivityLogPolicy.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\ActivityLog;
+use Illuminate\Foundation\Auth\User as AuthUser;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ActivityLogPolicy
+{
+    use HandlesAuthorization;
+    
+    public function viewAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ViewAny:ActivityLog');
+    }
+
+    public function view(AuthUser $authUser, ActivityLog $activityLog): bool
+    {
+        return $authUser->can('View:ActivityLog');
+    }
+
+    public function create(AuthUser $authUser): bool
+    {
+        return $authUser->can('Create:ActivityLog');
+    }
+
+    public function update(AuthUser $authUser, ActivityLog $activityLog): bool
+    {
+        return $authUser->can('Update:ActivityLog');
+    }
+
+    public function delete(AuthUser $authUser, ActivityLog $activityLog): bool
+    {
+        return $authUser->can('Delete:ActivityLog');
+    }
+
+    public function deleteAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('DeleteAny:ActivityLog');
+    }
+
+    public function restore(AuthUser $authUser, ActivityLog $activityLog): bool
+    {
+        return $authUser->can('Restore:ActivityLog');
+    }
+
+    public function forceDelete(AuthUser $authUser, ActivityLog $activityLog): bool
+    {
+        return $authUser->can('ForceDelete:ActivityLog');
+    }
+
+    public function forceDeleteAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ForceDeleteAny:ActivityLog');
+    }
+
+    public function restoreAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('RestoreAny:ActivityLog');
+    }
+
+    public function replicate(AuthUser $authUser, ActivityLog $activityLog): bool
+    {
+        return $authUser->can('Replicate:ActivityLog');
+    }
+
+    public function reorder(AuthUser $authUser): bool
+    {
+        return $authUser->can('Reorder:ActivityLog');
+    }
+}
